21b022d3d6fe270beaa31cd1ea0f58257516c2f8
Thiago <thiagoakimoto2006@gmail.com>
2024-11-01 12:36:04 -0300

    fiz o unicast e o broadcast

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,4 @@
-import socket 
+import socket
 
 #IP e PORTA do servidor que queremos nos conectar
 HOST = '127.0.0.1'
@@ -6,14 +6,16 @@ PORTA = 9999
 socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #cliente solicita conexão
+
 socket_cliente.connect((HOST, PORTA))
 print(5*"-" + "CHAT INICIADO" + 5*"-")
-#INformar o nome ao servidor
-nome = input("INforme seu nome para entrar no chat:")
+
+#Vamos informar o nome
+nome = input("Informe seu nome para entrar no chat: ")
 socket_cliente.sendall(nome.encode())
 
-#loop de envio de dados
+#Loop de envio de dados
 while True:
     mensagem = input('')
-    #Para enviar damos o encode() para transformar em bytes str -> bytes
-    socket_cliente.sendall(mensagem.encode())
+#Para enviar damos o encode() para transformar em bytes str -> bytes
+socket_cliente.sendall(mensagem.encode())
--- Servidor.py
+++ Servidor.py
@@ -1,57 +1,61 @@
-import socket 
+import socket
 import threading
-#função que recebe mensagem do cliente
-def  recebe_dados (sock_cliente, endereco):
-    #receber o nome (máx 50 carac) decotes() bytes -> str
+#Função para recebimento de mensagem do cliente
+def recebe_dados(sock_cliente, endereco):
+#Receber o nome (máximo 50 caracteres) decode() bytes -> str
     nome = sock_cliente.recv(50).decode()
-    print(f"Conexão bem sucedida com {nome} via endereço: {endereco}")
+    print(f"Conexão bem sucedida com {nome} via endereço: {endereco}") 
+    lista_clientes[endereco] = nome
     while True:
-            try: 
+            try:
                 mensagem = sock_cliente.recv(1024).decode()
                 print(f"Cliente >> {mensagem}")
             except:
-                 print("Erro ao receber mensagem... fechamos")
-                 sock_cliente.close()
-                 return
-#implementar broadcast      
-def broadcast(lista_clientes):
-     pass
+                print("Erro ao receber mensagem... fechando")
+                sock_cliente.close()
+            return
+# Função para envio de mensagem a todos os clientes conectados (broadcast)
+def broadcast(mensagem,nome ):
+    for cliente, nome in lista_clientes.items():
+            try:
+                cliente[0].sendall(mensagem.encode())
+            except Exception as e:
+                print(f"Erro ao enviar para {nome}: {e}")
 #implementar unicast
-def unicast(cliente): 
-     pass
+def unicast(mensagem, nome):
+    try:
+        nome[0].sendall(mensagem.encode())
+    except Exception as e:
+        print(f"Erro ao enviar mensagem unicast para {lista_clientes[nome]}: {e}")
+
+    
 #implementar remoção do cliente quando ele sair do chat
-def remover(cliente):
-     pass
-
-
-
-#endereço utilizado para o servidor
+def remover(mensagem, lista_cliente):
+    pass
+#Endereço que será utilizado para o servidor
 HOST = '127.0.0.1'
 PORTA = 9999
 
-lista_clietes = []
+lista_clientes = {}
+
 sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#fazemos o bind  - > Link do ip:porta
-sock_server.bind((HOST, PORTA))
 
-#abrimos o servidor para o nome de escuta
 
+#fazemos o bind -> LINK do IP:PORTA
+sock_server.bind((HOST,PORTA))
+
+#abrimos o servidor para o mode de escuta
 sock_server.listen()
-print(f'O servidor {HOST}: {PORTA} está aguardando conexões...')
-
+print(f"O servidor {HOST}:{PORTA} está aguardando conexões...")
 
 #O servidor deve aceitar uma conexão solicitada pelo cliente
-#Criar um loop para o servidor acerar várias conexões
+#Vamos criar um loop para o servidor acertar várias conexões
 while True:
     sock_conn, ender = sock_server.accept()
     #Conexão com sucesso, vamos receber dados
-    #CRIamos um loop para recebimento das mensagens do cliente
+    #Criamos um loop para recebimento das mensagens do cliente
     #Criamos uma thread para a função recebe_dados(sock_conn, ender)
-    #sock_con é o socket de conexão vindo do cliente
-    thread_cliente = threading.Thread(target=recebe_dados, args=[sock_conn,ender])
+    #sock_conn é o socket de conexão vindo do cliente
+    thread_cliente = threading.Thread(target=recebe_dados, args=[sock_conn, ender])
     thread_cliente.start()
-
-
-
-
